b851ecf1a7e828d5355740782351eb79c319e5ff
Apolos666 <quangprox121@gmail.com>
2024-12-20 15:07:53 +0700

    Installing third party libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,14 @@ android {
 }
 
 dependencies {
-
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("commons-io:commons-io:2.11.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.microsoft.signalr:signalr:6.0.1")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("io.reactivex.rxjava3:rxjava:3.1.5")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
